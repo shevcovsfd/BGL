a56846f82150e47b44f99e80007e40a302623979
Danila Shevcov <53534279+shevcovsfd@users.noreply.github.com>
2020-11-08 15:58:36 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, url_for, request, make_response
+import random
+import datetime
+
+CHARSET = "qpzry9x8gf2tvdw0s3jn54khce6mua7l"
+
+
+def bech32_polymod(values):
+    """Internal function that computes the Bech32 checksum."""
+    generator = [0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3]
+    chk = 1
+    for value in values:
+        top = chk >> 25
+        chk = (chk & 0x1ffffff) << 5 ^ value
+        for i in range(5):
+            chk ^= generator[i] if ((top >> i) & 1) else 0
+    return chk
+
+
+def bech32_hrp_expand(hrp):
+    """Expand the HRP into values for checksum computation."""
+    return [ord(x) >> 5 for x in hrp] + [0] + [ord(x) & 31 for x in hrp]
+
+
+def bech32_verify_checksum(hrp, data):
+    """Verify a checksum given HRP and converted data characters."""
+    return bech32_polymod(bech32_hrp_expand(hrp) + data) == 1
+
+
+def bech32_create_checksum(hrp, data):
+    """Compute the checksum values given HRP and data."""
+    values = bech32_hrp_expand(hrp) + data
+    polymod = bech32_polymod(values + [0, 0, 0, 0, 0, 0]) ^ 1
+    return [(polymod >> 5 * (5 - i)) & 31 for i in range(6)]
+
+def bech32_encode(hrp, data):
+    """Compute a Bech32 string given HRP and data values."""
+    combined = data + bech32_create_checksum(hrp, data)
+    return hrp + '1' + ''.join([CHARSET[d] for d in combined])
+
+
+data = [0]
+for i in range(32):
+    n = random.randint(0,31)
+    data.append(n)
+
+addres = bech32_encode("bgl", data)
+app = Flask(__name__)
+   
+@app.route('/', methods=['GET','POST'])
+def mainSc():
+    privateK = request.form.get('prK')
+    if request.method == 'POST':
+        print(privateK)
+        print(addres)
+    return render_template("index.html")
+@app.route('/', methods=['GET','POST'])
+def mainScr():
+    return render_template("main_Screen.html", message = addres)
+
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
